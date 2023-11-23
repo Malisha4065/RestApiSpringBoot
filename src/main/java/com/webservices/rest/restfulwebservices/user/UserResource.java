ac73ca69f61bee96251dddd04ad124f7454a9a46
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-11-23 23:57:16 +0530

    swagger ui added

--- src/main/java/com/webservices/rest/restfulwebservices/user/UserResource.java
+++ src/main/java/com/webservices/rest/restfulwebservices/user/UserResource.java
@@ -1,11 +1,11 @@
 package com.webservices.rest.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -29,8 +29,13 @@ public class UserResource {
         return user;
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
+
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
